71636df164b4ea2607bd1a7258e64f5df938f9b0
rayeongu <rayeongu@gmail.com>
2026-05-13 01:09:50 +0900

    feat: AOP 적용

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -21,6 +21,11 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+    /*@Bean
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }*/
+
 /*    @Bean
     public MemberRepository memberRepository() {
         *//*return new MemoryMemberRepository();*//*
