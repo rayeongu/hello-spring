642bec08641e7f6d767d991839b92b9149bdd369
rayeongu <rayeongu@gmail.com>
2026-05-13 00:17:02 +0900

    feat: 스프링 데이터 JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,9 +1,7 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,23 +9,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository() {
-        /*return new MemoryMemberRepository();*/
-        /*return new JdbcMemberRepository(dataSource);*/
-        /*return new JdbcTemplateMemberRepository(dataSource);*/
-        return new JpaMemberRepository(em);
-    }
+        *//*return new MemoryMemberRepository();*//*
+     *//*return new JdbcMemberRepository(dataSource);*//*
+     *//*return new JdbcTemplateMemberRepository(dataSource);*//*
+     *//*return new JpaMemberRepository(em);*//*
+    }*/
 }
